4b57cd078652a4ed00d1364f20d36d49d4e801ef
Gil Junqueira <gjunqueira@msn.com>
2021-12-12 08:50:45 -0600

    revised impseq

--- src/MySignalProcessing.jl
+++ src/MySignalProcessing.jl
@@ -21,13 +21,15 @@ This function has the form of :
          = 0 if n!=0
 
 # parameters
-    s:: signal
+    `start:: Int` : start of the impulse sequence n (horizontal axis)
+    `stop:: Int` : stop of the impulse sequence n (horizontal axis)
+        
 
 # returns
     s::signal
 """
 function impseq(start::Int, stop::Int)
-    s = signal(0.0, start,stop);
+    s = signal(0.0, start,stop); #constructor for initial signal
 
     for x in 1:length(s.n)
         if s.n[x] == 0
